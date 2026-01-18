21cd2603310808131c961363ef94c5b16c5ebccc
Al-Amin Islam <jimalamin7@gmail.com>
2026-01-18 23:20:29 +0600

    route

--- Vehicle Rental/src/modules/vehicles/vehicles.route.ts
+++ Vehicle Rental/src/modules/vehicles/vehicles.route.ts
@@ -4,9 +4,10 @@ import auth from "../../middleware/auth";
 
 const routes = express.Router();
 
-routes.post("/", auth("admin"), vehiclesController.createVehicles);
 routes.get("/", vehiclesController.getVehicles);
 routes.get("/:vehicleId", vehiclesController.getSingleVehicles);
+
+routes.post("/", auth("admin"), vehiclesController.createVehicles);
 routes.put("/:vehicleId", auth("admin"), vehiclesController.updateVehicles);
 routes.delete("/:vehicleId", auth("admin"), vehiclesController.deleteVehicles);
 
